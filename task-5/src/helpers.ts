591bf6d284518b8451964eabb20d74fc42adeafc
Stefan Genov <genov.stefan@yahoo.com>
2021-12-23 17:51:56 +0200

    refactor: remove helper fn unnecessary param, changed pool initialization to use contract address

--- task-5/src/helpers.ts
+++ task-5/src/helpers.ts
@@ -1,12 +1,14 @@
 
 import { Pool } from '../generated/schema'
 import { ZERO_BI } from "./constants"
+import { HEGIC_CALL_ADDRESS } from './constants';
+import { HegicCALL } from '../generated/HegicCALL/HegicCALL';
 
-export function getOrCreatePool(id: string): Pool {
-  let pool = Pool.load(id);
+export function getOrCreatePool(): Pool {
+  let pool = Pool.load(HEGIC_CALL_ADDRESS);
 
   if (pool == null) {
-    pool = new Pool(id);
+    pool = new Pool(HEGIC_CALL_ADDRESS);
     pool.name = "";
     pool.cumulativePremium = ZERO_BI;
     pool.cumulativeSettlementFees= ZERO_BI;
